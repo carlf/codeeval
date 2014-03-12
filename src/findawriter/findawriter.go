18a52ed152beb209a5125ffec7557ff1da4ec8a3
Carl Flippin <carlf@photocarl.org>
2014-03-11 19:16:28 -0700

    Finished findawriter

--- src/findawriter/findawriter.go
+++ src/findawriter/findawriter.go
@@ -4,14 +4,25 @@ import "fmt"
 import "log"
 import "bufio"
 import "os"
+import "strings"
+import "strconv"
 
 func main() {
-    file, err := os.Open(os.Args[1])
-    if err != nil {
-        log.Fatal(err)
-    }   
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-     }   
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		data := strings.Split(line, "| ")
+		letters := strings.Split(data[0], "")
+		indices := strings.Split(data[1], " ")
+		for _, n := range indices {
+			x, _ := strconv.Atoi(n)
+			fmt.Printf("%s", letters[x-1])
+		}
+		fmt.Printf("\n")
+	}
 }
